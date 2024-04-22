c0fd58e8ec9172d3d72310a2bb2c7763519950ad
Bishop121 <obidikeemmanuel@outlook.com>
2024-04-22 19:19:21 +0100

    update

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     import sys
@@ -8,4 +7,3 @@ def safe_print_integer_err(value):
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
-
