eabc055df7ac3a32f54b0a8a3939ae1da8619a3d
Bishop121 <obidikeemmanuel@outlook.com>
2024-04-22 18:42:10 +0100

    update

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/python3
+""" a file that prints safely"""
+
+
+def safe_print_list(my_list=[], x=0):
+    sum_total = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            sum_total += 1
+        except IndexError:
+            break
+    print()
+    return(sum_total)
+
